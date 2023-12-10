aa10c6eae1635fbb1d182dbb4580138181558f7d
YG <143325418+VecnaJS@users.noreply.github.com>
2023-12-10 21:37:22 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -56,7 +56,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/yigiitgunes"
+                href="https://www.instagram.com/22bilinmeyenlidenklem"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
